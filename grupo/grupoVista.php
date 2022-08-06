02598d4301e53d1230038e47acd752ab66fec29f
AlejandroV2204 <jvallarinocruz@gmail.com>
2022-08-06 15:25:45 -0500

    Se acomodaron los iconos de los botones, de manera que se vean más estéticos.

--- grupo/grupoVista.php
+++ grupo/grupoVista.php
@@ -92,9 +92,9 @@
                                                                       <td> $fila[3] </td>
                                                                       <td> $fila[4] </td>
                                                                       <td> $estado </td>    
-                                                                      <td class=\"td-actions\"><a  data-toggle=\"modal\" href=\"#modalEditar\" onclick=\"agregarForm('$datos');
-                                                                      \" class=\"btn btn-small btn-info\"><i class=\"feather icon-edit\"></i></a>
-                                                                      <a href=\"#modalEliminar\" onclick=\"agregarForm('$datos');
+                                                                      <td class=\"td-actions\"><a  data-toggle=\"modal\" href=\"#modalEditar\" style=\"width: 55px\" style=\"height: 40px\" onclick=\"agregarForm('$datos');
+                                                                      \" class=\"btn btn-info\"><i class=\"feather icon-edit\"></i></a>
+                                                                      <a href=\"#modalEliminar\" style=\"width: 55px\" style=\"height: 40px\" onclick=\"agregarForm('$datos');
                                                                       \" data-toggle=\"modal\" class=\"btn btn-danger btn-small\"><i class=\"feather icon-trash\"> </i></a></td>
                                                                   </tr>";
 

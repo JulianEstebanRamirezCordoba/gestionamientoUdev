367d6cf5107270a31faff1334230c5f5071294ca
AlejandroV2204 <jvallarinocruz@gmail.com>
2022-08-18 14:56:48 -0500

    Se eliminarón los comentarios de la vista y el controlador de los grupos

--- grupo/grupoControlador.php
+++ grupo/grupoControlador.php
@@ -10,17 +10,17 @@ $nombre_grupo = filter_input(INPUT_POST, 'nombre_grupo');
 $codigo_grupo = filter_input(INPUT_POST, 'codigo_grupo');
 $ciclo_grupo = filter_input(INPUT_POST, 'ciclo_grupo');
 $cantidadEstudiantes_grupo = filter_input(INPUT_POST, 'cantidadEstudiantes_grupo');
-$estado = '1';//por defecto viene en 1 que es activo y 0 es inactivo
+$estado = '1';
 
 
-//guardar
- if(isset($_POST['guardarGrupo'])){
+
+if(isset($_POST['guardarGrupo'])){
 echo "metodo guardar"; 
-//$campos es el nombre de los campos tal cual aparece en la base de datos
+
 $camposInsert = array("gru_nombre", "gru_codigo", "gru_ciclo", "gru_cantidadEstudiantes", "gru_estado");
-//$valores son los valores a almacenar
+
 $valoresInsert = array("$nombre_grupo", "$codigo_grupo", "$ciclo_grupo", "$cantidadEstudiantes_grupo", "$estado");
-//la funcion insertar recive el nombre de la tabla y los dos arrays de campos y valores
+
 $nombreTabla = "carrera";
 
 $utilModelo -> insertarDatos($nombreTabla, $camposInsert, $valoresInsert) ;
@@ -29,18 +29,16 @@ echo "si funciono";
 $_SESSION['mensajeOk']="Accion realizada";
     header('Location:grupoVista.php');
 
-
-//modificar
- }else if(isset($_POST['modificarGrupo'])){
+}else if(isset($_POST['modificarGrupo'])){
     echo "modificar <br>"; 
 
     $id = $_POST['id'];
 
-   //$campos es el nombre de los campos tal cual aparece en la base de datos
+
 $camposActualizar = array("gru_nombre", "gru_codigo", "gru_ciclo", "gru_cantidadEstudiantes");
-//$valores son los valores a almacenar
+
 $valoresActualizar = array("$nombre_grupo", "$codigo_grupo", "$ciclo_grupo", "$cantidadEstudiantes_grupo");
-//la funcion insertar recibe el nombre de la tabla y los dos arrays de campos y valores
+
 $nombreTabla = "grupo";
 $campoCondicion = "gru_id";
 $condiconIgual = "$id";
@@ -77,7 +75,7 @@ header('Location:grupoVista.php');
             $_SESSION['mensajeOk']="Accion realizada"; 
             header('Location:grupoVista.php');
     
-       }
+    }
 
         exit();
 

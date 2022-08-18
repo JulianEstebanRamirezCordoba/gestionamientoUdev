17a090f9c955b4aa7451190f2d0ebfb340fcbec7
Jeronimo312725 <marinjeronimo44@gmail.com>
2022-08-18 15:03:22 -0500

    Se suben datos depurados

--- salas/salaControlador.php
+++ salas/salaControlador.php
@@ -8,65 +8,45 @@ $utilModelo = new utilModelo();
 
 $sal_nombre = filter_input(INPUT_POST, 'sal_nombre');
 $sal_capacidadMaxima = filter_input(INPUT_POST, 'sal_capacidadMaxima');
-$estado = '1';//por defecto viene en 1 que es activo y 0 es inactivo
+$estado = '1';
 
+if(isset($_POST['guardarSala'])){
+    $camposInsert = array("sal_nombre", "sal_capacidadMaxima","sal_estado");
+    $valoresInsert = array("$sal_nombre", "$$sal_capacidadMaxima","$estado");
+    $nombreTabla = "sala";
 
+    $utilModelo -> insertarDatos($nombreTabla, $camposInsert, $valoresInsert) ;
 
-//guardar
- if(isset($_POST['guardarSala'])){
-
-//$campos es el nombre de los campos tal cual aparece en la base de datos
-$camposInsert = array("sal_nombre", "sal_capacidadMaxima","sal_estado");
-//$valores son los valores a almacenar
-$valoresInsert = array("$sal_nombre", "$$sal_capacidadMaxima","$estado");
-//la funcion insertar recive el nombre de la tabla y los dos arrays de campos y valores
-$nombreTabla = "sala";
-
-$utilModelo -> insertarDatos($nombreTabla, $camposInsert, $valoresInsert) ;
-
-echo "si funciono";
-$_SESSION['mensajeOk']="Accion realizada";
+    $_SESSION['mensajeOk']="Accion realizada";
     header('Location:salaVista.php');
 
-
-//modificar
- }else if(isset($_POST['modificarSala'])){
-    echo "modificar <br>";
-
+}else if(isset($_POST['modificarSala'])){
     $id = $_POST['id'];
+    $valoresActualizar = array("$sal_nombre", "$sal_capacidadMaxima");
+    $nombreTabla = "sala";
+    $campoCondicion = "sal_id";
+    $condiconIgual = "$id";
+    $camposActualizar = array("sal_nombre", "sal_capacidadMaxima");
 
-   //$campos es el nombre de los campos tal cual aparece en la base de datos
-$camposActualizar = array("sal_nombre", "sal_capacidadMaxima");
-//$valores son los valores a almacenar
-$valoresActualizar = array("$sal_nombre", "$sal_capacidadMaxima");
-//la funcion insertar recibe el nombre de la tabla y los dos arrays de campos y valores
-$nombreTabla = "sala";
-$campoCondicion = "sal_id";
-$condiconIgual = "$id";
+    $utilModelo -> actualizarDatos($nombreTabla, $camposActualizar, $valoresActualizar, $campoCondicion, $condiconIgual);
 
-$utilModelo -> actualizarDatos($nombreTabla, $camposActualizar, $valoresActualizar, $campoCondicion, $condiconIgual);
-$_SESSION['mensajeOk']="Accion realizada";
-header('Location:salaVista.php');
-
+    $_SESSION['mensajeOk']="Accion realizada";
+    header('Location:salaVista.php');
 
 }else{
+    $nombreTabla = "sala";
+    $valoresActualizar = array("0");
+    $camposActualizar = array("sal_estado");
+    $id = $_POST['idEliminar'];
 
-        echo "Eliminar";
+    $campoCondicion = "sal_id";
+    $condiconIgual = "$id";
     
-                $nombreTabla = "sala";
-                $valoresActualizar = array("0");
-                $camposActualizar = array("sal_estado");
-                $id = $_POST['idEliminar'];
-
-                $campoCondicion = "sal_id";
-                $condiconIgual = "$id";
+    $utilModelo -> actualizarDatos($nombreTabla, $camposActualizar, $valoresActualizar, $campoCondicion, $condiconIgual);
+    $_SESSION['mensajeOk']="Accion realizada";
+    header('Location:salaVista.php');
     
-                $utilModelo -> actualizarDatos($nombreTabla, $camposActualizar, $valoresActualizar, $campoCondicion, $condiconIgual);
-            $_SESSION['mensajeOk']="Accion realizada";
-             header('Location:salaVista.php');
-    
-       }
-
-        exit();
+}
+exit();
 
 ?>
--- salas/salaVista.php
+++ salas/salaVista.php
@@ -9,29 +9,14 @@
 <head>
 
     <title>SALAS</title>
-    <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 11]>
-    	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    	<![endif]-->
-    <!-- Meta -->
-    
-
-    <!-- Favicon icon -->
-   
-
 </head>
     </header>
-    <!-- [ Header ] end -->
-    <!-- [ Main Content ] start -->
     <section class="pcoded-main-container">
         <div class="pcoded-wrapper">
             <div class="pcoded-content">
                 <div class="pcoded-inner-content">
                     <div class="main-body">
                         <div class="page-wrapper">
-                            <!-- [ breadcrumb ] start -->
                             <div class="page-header">
                                 <div class="page-block">
                                     <div class="row align-items-center">
@@ -48,22 +33,17 @@
                                     </div>
                                 </div>
                             </div>
-                            <!-- [ breadcrumb ] end -->
-                            <!-- [ Main Content ] start -->
                             <div class="row">
-                               
-                                <!-- [ dark-table ] start -->
                                 <div class="col-md-12">
                                     <div class="card">
                                         <div class="card-header">
-                                          <!-- Button trigger modal -->
                                           <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalGuardar">
                                             AÑADIR NUEVA SALA                                  
                                           </button>
 
                                         <div class="card-body table-border-style">
                                             <div class="table-responsive">
-                                                <table class="table table-white" border = "2">
+                                                <table class="table table-white" border="2">
                                                     <thead>
                                                         <tr>
                                                           <th scope="col">IDENTIFICADOR SALA</th>
@@ -135,17 +115,12 @@
                                         </div>
                                     </div>
                                 </div>
-                                <!-- [ dark-table ] end -->
-                            <!-- [ Main Content ] end -->
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- [ Main Content ] end -->
-
-    <!-- Inicio Modal Guardar -->
 <div class="modal fade" id="modalGuardar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -219,9 +194,8 @@
 
   </form>
 </div>
-<!-- Fin Modal Editar -->
 
-<!-- Inicio Modal Editar -->
+
 <div class="modal fade" id="modalDelete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -258,55 +232,6 @@
   </form>
 </div>
 
-    <!-- Warning Section start -->
-    <!-- Older IE warning message -->
-    <!--[if lt IE 11]>
-        <div class="ie-warning">
-            <h1>Warning!!</h1>
-            <p>You are using an outdated version of Internet Explorer, please upgrade
-               <br/>to any of the following web browsers to access this website.
-            </p>
-            <div class="iew-container">
-                <ul class="iew-download">
-                    <li>
-                        <a href="http://www.google.com/chrome/">
-                            <img src="../assets/images/browser/chrome.png" alt="Chrome">
-                            <div>Chrome</div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.mozilla.org/en-US/firefox/new/">
-                            <img src="../assets/images/browser/firefox.png" alt="Firefox">
-                            <div>Firefox</div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://www.opera.com">
-                            <img src="../assets/images/browser/opera.png" alt="Opera">
-                            <div>Opera</div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.apple.com/safari/">
-                            <img src="../assets/images/browser/safari.png" alt="Safari">
-                            <div>Safari</div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
-                            <img src="../assets/images/browser/ie.png" alt="">
-                            <div>IE (11 & above)</div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <p>Sorry for the inconvenience!</p>
-        </div>
-    <![endif]-->
-    <!-- Warning Section Ends -->
-
-    <!-- Required Js -->
-	<!-- Required Js -->
 	<script src="../assets/js/vendor-all.min.js"></script>
 	<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="../assets/js/pcoded.min.js"></script>
